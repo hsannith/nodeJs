2c2b3bd080c1f1a4a0becd387f0cef512a161256
hitesh sannithi <hsannith@asu.edu>
2018-12-22 05:21:54 -0700

    MVC and file management

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const http=require('http');
+const path = require('path');
 
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const express=require('express');
+const pagenotfound=require('./controllers/404controller')
 
-const app=express();
+const app = express();
 
-const server=http.createServer(app);
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-server.listen(3000);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use(pagenotfound.error);
+
+app.listen(3000);
